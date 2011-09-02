7d921c9241e07d918d9a7b2f47adb443ef93c76a
Garima Singh <garima.slideshare@gmail.com>
2011-09-02 21:34:22 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GulatiPunjabiSwad::Application.config.secret_token = '614fc3c6e0fcff73682dac5d1ef464ea1bcfa6f5e875ae4acc79150f26635a1584fb98ac64ec0fd97cdead520a5d4ee3a2ff596e682d2bf3c056126505d6791d'
